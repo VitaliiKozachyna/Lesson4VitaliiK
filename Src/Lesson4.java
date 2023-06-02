439a24f21268a8fd58386a03f10eb338e1f39a44
VitaliiKozachyna <v.kozachyna@gmail.com>
2023-06-02 20:40:11 +0300

    Second commit

--- Src/Lesson4.java
+++ Src/Lesson4.java
@@ -1,5 +1,5 @@
 public class Lesson4 {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git. I'm Vitalii Kozachyna.");
     }
 }
